e0e1303d671e9d541896ded523e187abda88ab6a
unknown <ilieciprian18@yahoo.com>
2023-02-03 18:22:49 +0200

    commit

---
+++ src/MainApplicationUI/MainFoodDeliveryApplication.java
@@ -0,0 +1,40 @@
+package MainApplicationUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainFoodDeliveryApplication extends Application {
+
+private static Stage stg;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        stg = primaryStage;
+        primaryStage.setResizable(false);
+
+        Parent root = FXMLLoader.load(getClass().getResource("../MainApplicationUI/sample.fxml"));
+        primaryStage.setScene(new Scene(root, 825, 545));
+        primaryStage.setTitle("Food Delivery Application");
+       // primaryStage.getIcons().add(new Image("ApplicationUI/Images/icon.png"));
+        primaryStage.show();
+
+    }
+
+    public void changeScene(String fxml) throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        stg.getScene().setRoot(pane);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+}
---
+++ src/Services/RestaurantService.java
@@ -0,0 +1,14 @@
+package Services;
+
+import Classes.Restaurant;
+
+import java.util.Vector;
+
+public class RestaurantService {
+
+    public Vector<Restaurant> restaurante;
+
+    public RestaurantService(Vector<Restaurant> restaurante) {
+        this.restaurante = restaurante;
+    }
+}
